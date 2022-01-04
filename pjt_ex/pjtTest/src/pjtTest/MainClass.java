a8622b8215c1141694e2f18625e59e633085553a
임상택 <>
2022-01-04 22:33:51 +0900

    변수 선언

---
+++ pjt_ex/pjtTest/src/pjtTest/MainClass.java
@@ -0,0 +1,13 @@
+package pjtTest;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		int i = 10;
+		int j;
+		j = 100;
+		i = 300;
+		
+		System.out.println("i + j = " + (i+j));
+	}
+}
